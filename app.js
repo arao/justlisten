04a7278ed7cfcd0981670ec6bc8f56008e04c26e
akhilesh <akhileshrao19@gmail.com>
2018-12-05 00:40:01 +0530

    cors added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+let cors = require('cors');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
